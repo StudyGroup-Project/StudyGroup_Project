b438e8b7565dc9ffc5db9ed773660b026343a672
sihyeong <tlgud119@gamil.com>
2025-09-20 21:35:50 +0900

    Test(backend): 통합 테스트 코드 수정 및 필요 없는 class 제거

--- backend/src/test/java/com/study/focus/announcement/AnnouncementIntegrationTest.java
+++ backend/src/test/java/com/study/focus/announcement/AnnouncementIntegrationTest.java
@@ -1,10 +1,10 @@
 package com.study.focus.announcement;
 
 import com.study.focus.account.domain.User;
+import com.study.focus.account.dto.CustomUserDetails;
 import com.study.focus.account.repository.UserRepository;
 import com.study.focus.announcement.domain.Announcement;
 import com.study.focus.announcement.repository.AnnouncementRepository;
-import com.study.focus.config.MockCustomUser;
 import com.study.focus.study.domain.*;
 import com.study.focus.study.repository.StudyMemberRepository;
 import com.study.focus.study.repository.StudyRepository;
@@ -20,42 +20,49 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Transactional
 @AutoConfigureMockMvc
-
 public class AnnouncementIntegrationTest {
 
-    @Autowired private MockMvc mockMvc;
-    @Autowired private UserRepository userRepository;
-    @Autowired private StudyRepository  studyRepository;
-    @Autowired private StudyMemberRepository studyMemberRepository;
-    @Autowired private AnnouncementRepository announcementRepository ;
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private StudyRepository studyRepository;
+    @Autowired
+    private StudyMemberRepository studyMemberRepository;
+    @Autowired
+    private AnnouncementRepository announcementRepository;
 
 
-   private Study study1;
-   private  Study study2;
-   private  User user1;
+    private Study study1;
+    private Study study2;
+    private User user1;
 
     //임시 데이터 삽입
     @BeforeEach
-    void setUp(){
-         user1 = userRepository.save(User.builder().trustScore(30L).lastLoginAt(LocalDateTime.now()).build());
+    void setUp() {
+        user1 = userRepository.save(User.builder().trustScore(30L).lastLoginAt(LocalDateTime.now()).build());
         study1 = studyRepository.save(Study.builder().maxMemberCount(30).recruitStatus(RecruitStatus.OPEN).build());
         study2 = studyRepository.save(Study.builder().maxMemberCount(30).recruitStatus(RecruitStatus.OPEN).build());
-        StudyMember  studyMember1 = studyMemberRepository.save(StudyMember.builder().user(user1).study(study1).exitedAt(LocalDateTime.now().plusMonths(1)).
+        StudyMember studyMember1 = studyMemberRepository.save(StudyMember.builder().user(user1).study(study1).exitedAt(LocalDateTime.now().plusMonths(1)).
                 role(StudyRole.MEMBER).status(StudyMemberStatus.JOINED).build());
-        StudyMember  studyMember2 = studyMemberRepository.save(StudyMember.builder().user(user1).study(study2).exitedAt(LocalDateTime.now().plusMonths(1)).
+        StudyMember studyMember2 = studyMemberRepository.save(StudyMember.builder().user(user1).study(study2).exitedAt(LocalDateTime.now().plusMonths(1)).
                 role(StudyRole.MEMBER).status(StudyMemberStatus.JOINED).build());
 
-         announcementRepository.save(Announcement.builder().study(study1).author(studyMember1).title("TestTitle1").build());
+        announcementRepository.save(Announcement.builder().study(study1).author(studyMember1).title("TestTitle1").build());
         announcementRepository.save(Announcement.builder().study(study1).author(studyMember1).title("TestTitle2").build());
     }
+
     @AfterEach
-    void after(){
+    void after() {
         announcementRepository.deleteAll();
         studyMemberRepository.deleteAll();
         studyRepository.deleteAll();
@@ -64,10 +71,10 @@ public class AnnouncementIntegrationTest {
 
     @Test
     @DisplayName("성공: 스터디 멤버가 공지사항 목록을 성공적으로 조회")
-    @MockCustomUser
     void getAnnouncements_success() throws Exception {
         // when & then
-        mockMvc.perform(get("/api/studies/" + study1.getId() + "/announcements"))
+        mockMvc.perform(get("/api/studies/" + study1.getId() + "/announcements")
+                        .with(user(new CustomUserDetails(user1.getId())))) // setUp에서 생성된 user를 사용
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.length()").value(2))
                 .andExpect(jsonPath("$[0].title").value("TestTitle1"));
@@ -76,12 +83,22 @@ public class AnnouncementIntegrationTest {
 
     @Test
     @DisplayName("실패: 스터디 멤버가 아닐 경우 IllegalArgumentException이 발생")
-    @MockCustomUser
     void getAnnouncements_Fail_NotStudyMember() throws Exception {
-        mockMvc.perform(get("/api/studies/" + study2.getId() + "/announcements"))
+        mockMvc.perform(get("/api/studies/" + 11111L + "/announcements")
+                        .with(user(new CustomUserDetails(user1.getId()))))
                 .andExpect(status().isBadRequest());
     }
 
 
+    @Test
+    @DisplayName("성공: 빈 멤버 리스트 반환")
+    void getAnnouncements_success_IsEmptyList() throws Exception {
+        // when & then
+        mockMvc.perform(get("/api/studies/" + study2.getId() + "/announcements")
+                        .with(user(new CustomUserDetails(user1.getId())))) // setUp에서 생성된 user를 사용
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(0));
 
+    }
 }
+
