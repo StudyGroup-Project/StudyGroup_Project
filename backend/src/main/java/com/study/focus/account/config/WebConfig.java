2547dd31912fe21c99228eee3a1b3f1be416b3f1
HyeonCheol Lee <guscjf8921@gmail.com>
2025-11-13 18:30:25 +0900

    프론트 배포 리스트 추가(임시로 OAuth만 리다이렉트url변경)

--- backend/src/main/java/com/study/focus/account/config/WebConfig.java
+++ backend/src/main/java/com/study/focus/account/config/WebConfig.java
@@ -16,7 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174")); // 프론트 주소
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174",
+                "https://study-group-project-frontend.vercel.app/")); // 프론트 주소
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
