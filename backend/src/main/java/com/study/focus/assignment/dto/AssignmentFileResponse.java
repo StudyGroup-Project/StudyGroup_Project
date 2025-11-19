53fcb15ecfa2c9b9f05b4b0265bc8af0911a74d5
Jang seheon <tpgjs3121@naver.com>
2025-11-19 11:00:31 +0900

    fix: 과제관련 파일의 DTO형식 변경

--- backend/src/main/java/com/study/focus/assignment/dto/AssignmentFileResponse.java
+++ backend/src/main/java/com/study/focus/assignment/dto/AssignmentFileResponse.java
@@ -1,10 +1,16 @@
 package com.study.focus.assignment.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Builder
+@Setter
 @AllArgsConstructor
 public class AssignmentFileResponse {
-    String url;
+    private  Long fileId;
+    private String fileName;
+    private String url;
 }
