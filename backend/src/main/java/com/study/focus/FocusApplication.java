1b9ebc8aba2043878209e5bf2fab3ed5f51aac58
White_DeepDive <tlgud119@yu.ac.kr>
2025-11-20 22:01:17 +0900

    fix(server): 서버 타임존 변경
    - 현재 서버의 타임존을 따로 설정하지 않아, OS 타임존 설정을 따라감
    - EC2의 기본 타임존은 UTC로 설정되어 있기 때문에,
    설정한 스케줄러가 정상 작동하지 않는 문제 발생
    - 이를 해결하기 위해 서버의 타임존을 직접 명시

--- backend/src/main/java/com/study/focus/FocusApplication.java
+++ backend/src/main/java/com/study/focus/FocusApplication.java
@@ -1,5 +1,7 @@
 package com.study.focus;
 
+import java.util.TimeZone;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,11 +11,17 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @SpringBootApplication
 @EnableScheduling
+@Slf4j
 public class FocusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FocusApplication.class, args);
-		
 	}
+
+	public void setUp(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
+
 
 }
